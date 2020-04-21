94718730304851230fe668ce360d26bbe8b1b9c9
Charlie <charlierobinson080@gmail.com>
2020-04-21 21:28:15 +0100

    fix typor in push commands

--- python/build-docker.sh
+++ python/build-docker.sh
@@ -1,4 +1,4 @@
 cd $TRAVIS_BUILD_DIR/python
 docker build . -t $DOCKER_USERNAME/in-call-python:$TRAVIS_BRANCH
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH
+docker push $DOCKER_USERNAME/in-call-python:$TRAVIS_BRANCH
--- web/build-docker.sh
+++ web/build-docker.sh
@@ -1,4 +1,4 @@
 cd $TRAVIS_BUILD_DIR/web
 docker build . -t $DOCKER_USERNAME/in-call-web:$TRAVIS_BRANCH
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH
+docker push $DOCKER_USERNAME/in-call-web:$TRAVIS_BRANCH
